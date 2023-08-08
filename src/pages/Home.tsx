287d30d3c88d2c36ca0149c8d0e5f452d7f32b38
lmathis <lelemathis@gmail.com>
2023-08-08 18:44:09 -0500

    Added descriptions to project sections

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,19 @@
 import { useNavigate } from 'react-router-dom';
 import { useAppSelector } from '../hooks/typedHooks';
 
+import { useTheme, Card } from '@mui/material';
 import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 
 function HomePage() {
+  const isNarrow = useAppSelector((state) => state.ui.isNarrow);
   const navigate = useNavigate();
-  const isMobile = useAppSelector((state) => state.ui.isMobile);
+  const theme = useTheme();
+
+  document.title = 'Home | Lele Mathis Portfolio';
+
+  const m = isNarrow ? 1 : 2;
 
   const clickWeatherHandler = () => {
     navigate('weather');
@@ -24,48 +28,84 @@ function HomePage() {
       <Typography component='h1' variant='h4' color='primary'>
         Portfolio Website Home
       </Typography>
-      <Typography variant='body1' sx={{ m: 2 }}>
-        Hi, I'm Lele Mathis, a frontend software developer and data analyst.
+      <Typography variant='body1' sx={{ m: m }}>
+        Hi, I'm Lele Mathis, a frontend software developer and data analyst with
+        a background in Physics and Materials Science research. Please take a
+        look around!
       </Typography>
-      <Grid container>
-        <Grid item sx={{ m: 2 }}>
-          <Button
+      <Grid
+        container
+        direction={isNarrow ? 'column' : 'row'}
+        display='flex'
+        justifyContent='center'
+        sx={{ my: 2 }}
+      >
+        <Grid item xs={5}>
+          <Card
+            className='homeCard'
             onClick={clickWeatherHandler}
-            variant='outlined'
-            size='large'
-            style={{
-              textTransform: 'none',
-            }}
-            sx={{ p: 4 }}
+            sx={{ m: m, p: m, minHeight: '200px' }}
           >
-            <Typography>
-              Check out my weather app built using ReactJS with redux and
-              TypeScript
+            <Typography
+              component='h2'
+              variant='h5'
+              sx={{
+                fontWeight: 'bold',
+                color: theme.palette.secondary.dark,
+              }}
+            >
+              Check out my weather app
             </Typography>
-          </Button>
+
+            <Typography variant='subtitle1'>
+              built using react.js with redux and TypeScript
+            </Typography>
+            <ul>
+              <li>
+                Learn about current and forecasted weather around the world
+              </li>
+              <li>
+                Add locations you're interested in and save them in a profile to
+                look at later!
+              </li>
+            </ul>
+          </Card>
         </Grid>
-        <Grid item sx={{ m: 2 }}>
-          <Button
+        <Grid item xs={5}>
+          <Card
             onClick={clickDataHandler}
-            variant='outlined'
-            size='large'
-            style={{
-              textTransform: 'none',
-            }}
-            sx={{ p: 4 }}
+            className='homeCard'
+            sx={{ m: m, p: m, minHeight: '200px' }}
           >
-            <Typography>
-              Check out my data analytics projects using SQL, Excel, and Tableau
+            <Typography
+              component='h2'
+              variant='h5'
+              sx={{
+                fontWeight: 'bold',
+                color: theme.palette.secondary.dark,
+              }}
+            >
+              Check out my data analytics projects
             </Typography>
-          </Button>
+            <Typography variant='subtitle1'>
+              made using SQL, Excel, and Tableau
+            </Typography>
+            <Typography component='p' variant='body1'>
+              <ul>
+                <li>
+                  Learn about tornadoes, hydrogen fueling stations, and more!
+                </li>
+              </ul>
+            </Typography>
+          </Card>
         </Grid>
       </Grid>
     </>
   );
-  if (isMobile) {
+  if (isNarrow) {
     return <Box sx={{ m: 1 }}>{pageContent}</Box>;
   } else {
-    return <Paper sx={{ m: 2, p: 2 }}>{pageContent}</Paper>;
+    return <Box sx={{ m: 2, p: 2 }}>{pageContent}</Box>;
   }
 }
 
