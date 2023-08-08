40aa9c2e89117d50e4b04fa9f49d60a44e67460b
lmathis <lelemathis@gmail.com>
2023-08-08 14:57:25 -0500

    Don't show a day if data is missing

--- src/components/FiveDayForecast.tsx
+++ src/components/FiveDayForecast.tsx
@@ -1,10 +1,12 @@
-import { useAppSelector } from '../hooks/typedHooks';
-import { Card, Container, Grid } from '@mui/material';
+import { useAppSelector, useAppDispatch } from '../hooks/typedHooks';
+import { Grid } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import WeatherPoint from '../models/weatherPoint';
+import { uiActions } from '../store/store';
 
 const FiveDayForecast: React.FC<{ data: WeatherPoint[] }> = (props) => {
   const isMobile = useAppSelector((state) => state.ui.isMobile);
+  const dispatch = useAppDispatch();
 
   const stringDate = (date: Date) => {
     return date.toString().substring(0, 10);
@@ -14,7 +16,7 @@ const FiveDayForecast: React.FC<{ data: WeatherPoint[] }> = (props) => {
     {};
 
   //from https://stackoverflow.com/questions/40410470/highest-occurrence-in-an-array-or-first-selected
-  function modeString(array: string[]) {
+  function mostFrequent(array: string[]) {
     let map = array.map(function (a) {
       return array.filter(function (b) {
         return a === b;
@@ -39,29 +41,48 @@ const FiveDayForecast: React.FC<{ data: WeatherPoint[] }> = (props) => {
   const fiveDayWeather: JSX.Element[] = [];
 
   for (let date in weatherByDate) {
-    const weatherMode = modeString(weatherByDate[date].weather);
-    const iconMode = modeString(weatherByDate[date].icon);
-    fiveDayWeather.push(
-      <Grid item key={date}>
-        <img //only using 'day' icons
-          alt={weatherMode ? weatherMode : ''}
-          aria-labelledby='caption'
-          src={
-            isMobile
-              ? `https://openweathermap.org/img/wn/${iconMode}d.png`
-              : `https://openweathermap.org/img/wn/${iconMode}d@2x.png`
-          }
-        />
-        <Typography id='caption' variant={isMobile ? 'caption' : 'body1'}>
-          {date}: {weatherMode}
-        </Typography>
-        {/* <Typography variant='caption'>
+    const weatherMode = mostFrequent(weatherByDate[date].weather);
+    const iconMode = mostFrequent(weatherByDate[date].icon);
+    if (weatherMode && iconMode) {
+      fiveDayWeather.push(
+        <Grid item key={date}>
+          <img //only using 'day' icons
+            alt={weatherMode ? weatherMode : ''}
+            aria-labelledby='caption'
+            src={
+              isMobile
+                ? `https://openweathermap.org/img/wn/${iconMode}d.png`
+                : `https://openweathermap.org/img/wn/${iconMode}d@2x.png`
+            }
+          />
+          <Typography id='caption' variant={isMobile ? 'caption' : 'body1'}>
+            {date}: {weatherMode}
+          </Typography>
+          {/* <Typography variant='caption'>
           {weatherByDate[date].icon.map((val) => val)}
           <br />
           {weatherByDate[date].weather.map((val) => val)}
         </Typography> */}
-      </Grid>
-    );
+        </Grid>
+      );
+    } else if (weatherMode) {
+      fiveDayWeather.push(
+        <Grid item key={date}>
+          <Typography id='caption' variant={isMobile ? 'caption' : 'body1'}>
+            {date}: {weatherMode}
+          </Typography>
+        </Grid>
+      );
+    } else {
+      console.log('Weather forecast data missing for ' + date);
+      //   dispatch(
+      //     uiActions.showNotification({
+      //       status: 'warning',
+      //       title: 'Warning',
+      //       message: 'Weather forecast data missing for ' + date,
+      //     })
+      //   );
+    }
   }
   return (
     <Grid
